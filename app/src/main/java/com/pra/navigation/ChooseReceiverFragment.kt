8e1210da25471b8c22e6b5e8c8b1cd0d803504a4
pra-josh <42682579+pra-josh@users.noreply.github.com>
2023-03-04 12:12:07 +0530

    Implicit deeplink & Explicit deeplink

--- app/src/main/java/com/pra/navigation/ChooseReceiverFragment.kt
+++ app/src/main/java/com/pra/navigation/ChooseReceiverFragment.kt
@@ -1,10 +1,14 @@
 package com.pra.navigation
 
+import android.app.PendingIntent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavDeepLinkBuilder
 import androidx.navigation.fragment.findNavController
 import com.pra.navigation.databinding.FragmentChooseReceiverBinding
 
@@ -25,10 +29,18 @@ class ChooseReceiverFragment : Fragment(R.layout.fragment_choose_receiver) {
         super.onViewCreated(view, savedInstanceState)
 
         mBinding?.btnNext?.setOnClickListener {
+
             val receiverName = mBinding?.etReceiverName?.text.toString()
             if (receiverName.isEmpty()) {
                 return@setOnClickListener
             }
+
+            val pendinginIntent =
+                findNavController().createDeepLink().setGraph(R.navigation.nav_graph)
+                    .setDestination(R.id.sendCashFragment).setArguments(SendCashFragmentArgs(receiverName).toBundle())
+                    .createPendingIntent()
+
+            showNotification(pendinginIntent, receiverName)
             val action =
                 ChooseReceiverFragmentDirections.actionChooseReceiverFragmentToSendCashFragment(
                     receiverName
@@ -40,6 +52,16 @@ class ChooseReceiverFragment : Fragment(R.layout.fragment_choose_receiver) {
         mBinding?.btnCancel?.setOnClickListener {
             findNavController().popBackStack()
         }
+    }
+
+    private fun showNotification(pendinginIntent: PendingIntent, receiverName: String) {
+
+        val notification = NotificationCompat.Builder(requireActivity(), CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_notifications).setContentTitle("Complete Transaction")
+            .setContentText("Send Money to $receiverName").setContentIntent(pendinginIntent)
+            .setAutoCancel(true).build()
+
+        NotificationManagerCompat.from(requireActivity()).notify(12, notification)
     }
 
 
